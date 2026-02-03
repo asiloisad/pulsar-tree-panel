e0b01255c36531918128027d2e7fcbdf6eabaada
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-02-03 23:47:50 +0100

    update eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    atom: "readonly",
+    requestAnimationFrame: "readonly",
+  },
+  rules: {
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^etch$" }],
+  },
+};
